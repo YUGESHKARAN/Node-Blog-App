8f0770c610badda2d83bb60bcd9e252c0ad99be0
YUGESHKARAN <yugeshkaran01@gmail.com>
2025-02-01 15:24:39 +0530

    updated - 107 view page route changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,10 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/changePassword" element={<ChangePassword/>} />
           <Route path="/register" element={<Register />} />
+          <Route
+            path="/viewpage/:email/:id"
+            element={<ViewPage/>}
+          />
 
           {/* Protected Route */}
           <Route
@@ -39,10 +43,10 @@ function App() {
             element={<ProtectedRoute element={<ViewEditPost/>} />}
           />
 
-            <Route
+            {/* <Route
             path="/viewpage/:email/:id"
             element={<ProtectedRoute element={<ViewPage/>} />}
-          />
+          /> */}
            <Route
             path="/profile"
             element={<ProtectedRoute element={<ProfilePage/>} />}
